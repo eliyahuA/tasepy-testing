dfc7da18b1ca0d3b6d1bed81f67a126aa4fb667a
eliyahuA <45657774+eliyahuA@users.noreply.github.com>
2025-07-22 19:49:30 +0000

    setup tests

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,12 +4,17 @@ set -e
 echo "Installing tasepy and other Python dependencies..."
 python3 -m pip install -r requirements.txt
 
-echo "Authenticate with gh"
-export GH_TOKEN="$GH_TASEPY_READONLY"
-gh auth login
-gh auth status
-
 echo "clone tasepy"
-git clone https://github.com/eliyahuA/tasepy-testing.git
+git git clone https://x-access-token:${GH_TASEPY_READONLY}@github.com/eliyahuA/tasepy.git
+
+echo "Switch to 'to be tested' branch"
+(cd tasepy; git checkout alpha-release)
 
 echo "Installing development dependencies..."
+python3 -m pip install -r tasepy/dev-requirements.txt
+
+echo "Run unit tests"
+pytest tasepy/tests/unit
+
+echo "Run selected integration test"
+pytest tasepy/tests/integration/client/test_indices_online.py
