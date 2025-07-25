04a07645a646424fcb4786c567199450d4f75b27
eliyahuA <eliyahu.arlev@gmail.com>
2025-07-25 15:17:36 +0300

    fix

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,7 +5,7 @@ echo "Installing tasepy and other Python dependencies..."
 python3 -m pip install -r requirements.txt
 
 echo "clone tasepy"
-git git clone https://x-access-token:${GH_TASEPY_READONLY}@github.com/eliyahuA/tasepy.git
+git clone https://x-access-token:${GH_TASEPY_READONLY}@github.com/eliyahuA/tasepy.git
 
 echo "Switch to 'to be tested' branch"
 (cd tasepy; git checkout alpha-release)
